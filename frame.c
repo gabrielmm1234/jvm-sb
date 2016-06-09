40b1969ebb2c2cc39d7da64cf92acf49f11b8b93
gabrielmm1234 <gabrielmesquita@cjr.org.br>
2016-06-09 17:28:40 -0300

    Revert "Cria frame e pilha de frames - TODO inicializar pilha de operandos"
    This reverts commit 652e7c48a8a160b1e3ee9d9ba996bbde44718b21.

--- frame.c
+++ frame.c
@@ -4,38 +4,9 @@
 * e a stack de frames.
 */
 
-//Topo da pilha de frames.
-static struct stackFrame* topo = NULL;
-
 //Cria uma estrutura de frame e coloca na pilha. Precisa dos bytecodes
 //de uma referencia a constantPool e da classe que contém o método.
 void criaFrame(cp_info* cp, classFile* classe, code_attribute* code){
 	printf("Criando Frame.\n");
-
-	//Aloca espaço para o frame.
-	struct stackFrame* sf;
-	sf = malloc(sizeof(struct stackFrame));
-	sf->refFrame = malloc(sizeof(struct frame));
-
-	//Atualizando o topo da pilha.
-	sf->next = topo;
-	topo = sf;
-
-	//Preenche a estrutura com as informações do frame.
-	sf->refFrame->classe = classe;
-	sf->refFrame->constant_pool = cp;
-	sf->refFrame->max_locals = code->max_locals;
-	sf->refFrame->max_stack = code->max_stack;
-	sf->refFrame->code_length = code->code_length;
-	//Bytecodes.
-	sf->refFrame->code = code->code;
-	//pc começa na primeira instrução.
-	sf->refFrame->pc = 0;
-	//Aloca espaço para o fields do método. Max_locals -> máximo de fields.
-	sf->refFrame->fields = malloc(code->max_locals * sizeof(uint32_t));
-
-	//Atualiza o frame corrente. Será o frame em execução no momento.
-	frameCorrente = sf->refFrame;
-
-	//TODO criar pilha de operandos.
+	//TODO criar pilha de frames. E empilhar o frame construido aqui.
 }
--- frame.h
+++ frame.h
@@ -1,28 +1,4 @@
 
-//Por indicação do professor cada frame terá seu próprio PC
-//Cada método ao ser executado cria um frame próprio.
-struct frame{
-	uint32_t* fields;
-	cp_info* constant_pool;
-	classFile* classe;
-	uint16_t max_stack;
-	uint16_t max_locals;
-	uint32_t code_length;
-	uint8_t* code;
-	uint32_t pc;
-};
-
-//Pilha de referencias para estrutura frame.
-struct stackFrame{
-	struct frame* refFrame;
-	struct stackFrame* next;
-};
-
-//Salva o frame que está sendo executado.
-struct frame *frameCorrente;
-
-//Cria uma estrutura de frame e coloca na pilha. Precisa dos bytecodes
-//de uma referencia a constantPool e da classe que contém o método.
-void criaFrame(cp_info* cp, classFile* classe, code_attribute* code);
+void criaFrame();
 
 #include "frame.c"

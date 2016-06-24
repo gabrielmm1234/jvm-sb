9435e90f479f12bc627145ec52c2b1af85986c3d
Renato Rangel <renatorangelbomfim@gmail.com>
2016-06-24 17:23:55 -0300

    adicionei o dup_x1

--- instrucao.c
+++ instrucao.c
@@ -1877,8 +1877,29 @@ void dup(){
 	atualizaPc();
 }
 
+/**
+ * Instrução que duplica valor da pilha e adiciona de volta de forma que se repita de forma nao ordenada.
+ * @param void
+ * @return void
+ */
 void dup_x1(){
 
+	printf("Entrei no dup\n");
+	int32_t aux1, aux2;
+
+	//Desempilha
+	aux2 = pop_op();
+
+	aux1 = pop_op();
+
+	//Duplica
+	push(aux1);
+	printf("Valor %d empilhado\n",frameCorrente->pilha_op->operandos[frameCorrente->pilha_op->depth - 1]);
+	push(aux2);
+	printf("Valor %d empilhado\n",frameCorrente->pilha_op->operandos[frameCorrente->pilha_op->depth - 1]);
+	push(aux1);
+
+	atualizaPc();
 }
 void dup_x2(){
 
